08a28be70883c2d5f724a59a5b1a1daee1f23ce3
Raphael Silva <raphaelferras@gmail.com>
2018-10-22 13:46:50 -0400

    postman and express integration

---
+++ server/server.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then(
+    (doc) => {
+      res.send(doc);
+    },
+    (e) => {
+      res.send(e);
+    }
+  )
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+// var newUser = new User({
+//   email: 'raphael@test.com'
+// });
+//
+// newUser.save().then(
+//   (doc) => {
+//     console.log(JSON.stringify(doc  ));
+//   },
+//   (e) => {
+//     console.log('Unable to save the user ', e);
+//   }
+// )
+
+// var newTodo = new Todo({
+//   text: 'Challenge',
+//   completed: true,
+//   completedAt: 3
+// });
+
+// var newTodo = new Todo({
+//   text: ' 123'
+// });
+//
+// newTodo.save().then(
+//   (doc) => {
+//     console.log('Saved: ', doc);
+//   },
+//   (e) => {
+//     console.log('Unable to save the todo', e);
+//   }
+// );
