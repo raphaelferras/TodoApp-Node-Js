2f53a301bb2b3a07686d6fa78b655086d558e80e
Raphael Silva <raphaelferras@gmail.com>
2018-10-23 16:00:08 -0400

    Adding a new text to GET todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,22 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+var todos = [{
+    text: 'first test todo'
+},{
+  text: 'second test todo'
+},{
+  text: 'third test todo'
+},{
+  text: 'fourth test todo'
+}]
 
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => {
+    done();
+  });
 });
 
 describe('POST /todos', () => {
@@ -25,7 +38,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -43,9 +56,20 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(4);
           done();
         }).catch((e) => done(e));
       });
   });
-})
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(4);
+      }).end(done);
+  })
+});
