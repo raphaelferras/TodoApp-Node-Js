0972052b293707d442602263e13fcc9130c1f624
Raphael Silva <raphaelferras@gmail.com>
2018-11-05 12:10:10 -0500

    Add POST Users

--- server/server.js
+++ server/server.js
@@ -109,6 +109,23 @@ app.listen(PORT, () => {
 
 module.exports = {app};
 
+app.post('/users', (req, res) => {
+  var user = new User(_.pick(req.body, ['email','password','tokens.access','tokens.token']));
+  user.save().then(
+    () => {
+      return user.generateAuthToken();
+    }
+  ).then((token) => {
+      res.header('x-auth', token).send(user);
+    }
+  ).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+
+
 // var newUser = new User({
 //   email: 'raphael@test.com'
 // });
