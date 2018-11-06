5bc8c81398f9f428449ae1787829b649aa55dc5f
Raphael Ferras <raphaelferras@gmail.com>
2018-11-06 15:31:48 -0500

    Add a test case for git

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -297,3 +297,27 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELTE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+        var token = users[0].tokens[0].token
+        User.findOne({
+          '_id':users[0]._id
+        }).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }, (e) => {
+          return done(e)
+        });
+      });
+  });
+})
