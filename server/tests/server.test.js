ec1312e2af73f7360eaeaddb04ca754230ab6ea3
Raphael Ferras <raphaelferras@gmail.com>
2018-11-06 14:26:30 -0500

    Test login feature

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -214,7 +214,9 @@ describe('POST /users', () => {
           expect(user.password).toNotBe(password);
           expect(user.password).toBe(SHA256(JSON.stringify(password) + 'saltingthispasswordtomakeitmoresecure_by_raphael').toString());
           done();
-        });
+        }).catch((e) => {
+          done(e);
+        })
       });
   });
 
@@ -238,5 +240,60 @@ describe('POST /users', () => {
       .send({email, password})
       .expect(400)
       .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('Should log int user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        //console.log(res.headers['x-auth']);
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => {
+          done(e);
+        })
+      });
+  });
+
+  it('Should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[0].password
+      })
+      .expect(400)
+      .expect((res) => {
+        //console.log(res.headers['x-auth']);
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => {
+          done(e);
+        })
+      });
   });
 });
